b9a2b0144a029135df22a9ef28a5533a34f3a7e0
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-17 23:59:44 +0900

    Add files via upload

---
+++ source-and-image/assignment5/bar.c
@@ -0,0 +1,4 @@
+#include "a.h"
+void InBar(){
+	printf("Inbar() Called...\n");
+}
---
+++ source-and-image/assignment5/mtest.c
@@ -0,0 +1,9 @@
+#include "a.h"
+
+int main(int argc, char* argv[]){
+	InFoo();
+	InBoo();
+	InBar();
+
+	return 0;
+}
